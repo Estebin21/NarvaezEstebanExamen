e70c32e0e65c9fffb0d9d35ad67b14023383b6cd
Estebin21 <esteban.narvaez.acurio@udla.edu.ec>
2024-12-02 08:51:34 -0500

    recarga telefónica completada, binding corregido

--- NarvaezEstebanExamen/Models/RecargaTelefonica.cs
+++ NarvaezEstebanExamen/Models/RecargaTelefonica.cs
@@ -8,7 +8,7 @@ namespace NarvaezEstebanExamen.Models
 {
     public class RecargaTelefonica
     {
-        public string Id { get; set; }
+        public int Id { get; set; }
         public string Numero { get; set; }
         public string Nombre { get; set; }
     }
--- NarvaezEstebanExamen/Pagina1.xaml.cs
+++ NarvaezEstebanExamen/Pagina1.xaml.cs
@@ -1,9 +1,37 @@
+using NarvaezEstebanExamen.Interfaces;
+using NarvaezEstebanExamen.Models;
+using NarvaezEstebanExamen.Repositories;
+
 namespace NarvaezEstebanExamen;
 
 public partial class Pagina1 : ContentPage
 {
+	IRecargaTelefonicaRepository _recargaTelefonicaRepository;
+	RecargaTelefonica recarga = new RecargaTelefonica();
 	public Pagina1()
 	{
+		_recargaTelefonicaRepository = new RecargaTelefonicaArchivos();
 		InitializeComponent();
+
+		recarga = _recargaTelefonicaRepository.DevuelveRecargaTelefonica(1);
+		BindingContext = recarga;
+	}
+	private async void GuardarRecarga_Clicked(object sender, EventArgs e)
+	{
+		RecargaTelefonica recarga = new RecargaTelefonica
+		{
+			Nombre = editor_nombre.Text,
+			Numero = editor_numero.Text
+		};
+		bool guardar_recarga = _recargaTelefonicaRepository.CrearRecargaTelefonica(recarga);
+
+		if (guardar_recarga)
+		{
+			await DisplayAlert("Alerta", "Recarga guardada", "OK");
+		}
+		else
+		{
+            await DisplayAlert("Alerta", "Recarga NO guardada", "OK");
+        }
 	}
 }
