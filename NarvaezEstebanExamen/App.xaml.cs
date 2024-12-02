d77f16cbda0266e03ef52afe26f3b1191c31aace
laboratorio <laboratorio@UPOAULA10223>
2024-12-02 07:28:50 -0500

    Agregar archivos de proyecto.

---
+++ NarvaezEstebanExamen/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace NarvaezEstebanExamen
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ NarvaezEstebanExamen/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace NarvaezEstebanExamen
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
